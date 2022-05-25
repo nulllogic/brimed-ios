57fb01a6ad048d6fd7ab0bf29ee502ac666317cd
Vladimir Lukyanov <vladimir@liikyanov.com>
2022-05-25 17:49:23 +0300

    v0.0.2

---
+++ Brimed/BrimedApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrimedApp.swift
+//  Brimed
+//
+//  Created by Vladimir Lukyanov on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BrimedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
